79a5aefa7f39d45c6e618befc97ffc613d333878
milad <ozviatvagame@gmail.com>
2021-05-21 23:22:14 +0430

    add dashboard and logout

--- account/views.py
+++ account/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
@@ -26,3 +27,6 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 
+@login_required()
+def dashboard(request):
+    return render(request, 'account/dashboard.html', {'section': 'dashboard'})
